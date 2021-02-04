24d60e93eff3d292a786f4b9115793bb99036f4e
Dilasha Rimal <thagarimal@gmail.com>
2021-02-04 17:51:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,18 @@ import React from "react";
 import "./App.css";
 
 import Welcome from "./Components/welcome/Welcome";
-import Clock from "./Components/clock/Clock";
-import Contact from "./Components/contact/Contact";
 import Navigation from "./Components/navigation/Navigation";
 import { Route, Switch } from "react-router-dom";
-import pageNotFound from "./Components/404/PageNotFound";
+import Jeopardy from "./Components/jeopardy/Jeopardy";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={(props) => <Welcome {...props} name="Dilasha" />}
-        />
-        <Route path="/clock" component={Clock} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/welcome" component={Welcome} />
 
-        <Route>
-          {"404! "}
-          <pageNotFound />
-          {" page Not Found"}
-        </Route>
+      <Switch>
+        <Route exact path="/" component={Jeopardy} />
+        <Route path="/welcome/:name" component={Welcome} />
       </Switch>
     </div>
   );
